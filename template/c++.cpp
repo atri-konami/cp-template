a5816d00691d5d26ab945559b33d2a091c750358
atri-konami <freude1994@gmail.com>
2015-12-07 00:53:17 +0900

    modified template/c++.cpp

--- template/c++.cpp
+++ template/c++.cpp
@@ -3,10 +3,14 @@ using namespace std;
 
 #define all(x) (x).begin(),(x).end()
 #define REP(i,a,b) for(int i=(int)(a); i<(int)(b); i++)
+#define RREP(i,a,b) for(int i=(int)(a); i>=b; i--)
 #define rep(i,n) REP(i,0,n)
-#define pb push_back
+#define rrep(i,n) RREP(i,n,0)
+#define pb(x) push_back(x)
+#define mp(x,y) make_pair((x),(y))
 
 typedef long long ll;
+typedef unsigned long long ull;
 typedef pair<int,int> pii;
 typedef vector<int> vi;
 typedef vector<pii> vp;
@@ -16,9 +20,7 @@ typedef vector<pii> vp;
 template<class T>
 ostream& operator<<(ostream& os,const vector<T> v) {
     os << "[";
-    for(int i=0; i<v.size(); i++){
-        os << v[i] << ",";
-    } 
+    for(int i=0; i<v.size(); i++) os << v[i] << ",";
     os << "]";
     return os;
 }
